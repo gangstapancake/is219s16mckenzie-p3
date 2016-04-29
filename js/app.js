cbaef770fd4c69178258289b0fa6396e83410796
gangstapancake <dlm24@njit.edu>
2016-04-28 20:34:16 -0400

    Project 3 Parts 1,2, and 3

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var app = angular.module('IS219Gallery', []);
+
+	app.controller('HomeController', ['$scope', function($scope) {
+	$scope.title = “IMDB ” + $scope.owner + " \'s Top 8 Movies";
+	$scope.owner = “Dominic”;
+	$scope.githhub = "https://github.com/gangstapancake/is219s16mckenzie-p3"; 
+	}])
