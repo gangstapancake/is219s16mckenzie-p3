76e6694b38c19dac998e90ef2ea29590a739324d
gangstapancake <dlm24@njit.edu>
2016-04-28 20:58:48 -0400

    Add controller and Part 4

---
+++ js/homecontroller.js
@@ -0,0 +1,12 @@
+	app.controller('HomeController', ['$scope', function($scope) {
+	
+	$scope.title = “IMDB ” + $scope.owner + " \'s Top 8 Movies";
+	$scope.owner = “Dominic”;
+	$scope.githhub = "https://github.com/gangstapancake/is219s16mckenzie-p3"; 
+	
+	
+	
+	
+	
+	
+	}])
